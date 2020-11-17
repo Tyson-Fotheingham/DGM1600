e774224dd4ed7e5a729f1463dd73b8ef17183de2
Tyson Fotheringham <tyson.fotheringham@gmail.com>
2020-11-17 10:41:08 -0700

    fixed the films page!

--- starWarsFilms/main.js
+++ starWarsFilms/main.js
@@ -1,5 +1,5 @@
 import { films } from '../data/films.js'
-import {people} from '../data/people.js'
+import {getLastNumber} from '../utils/index.js'
 
 const main = document.querySelector ('main')
 
@@ -9,7 +9,10 @@ for (let i = 0; i< 7; i++) {
     let figImg = document.createElement('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCation = document.createElement('figCaption')
-    figCation.textContent = films[i].title
+
+    const foundFlim = films.find(film => getLastNumber(film.url) == (i + 1))
+
+    figCation.textContent = foundFlim.title
     
     figure.appendChild(figImg)
     figure.appendChild(figCation)
