049c8785f345d58ebdcc5ec8639a78c73c72bc20
Tyson Fotheringham <tyson.fotheringham@gmail.com>
2020-12-03 15:46:38 -0700

    added pokeButtons

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -14,48 +14,80 @@ async function getAPIData(url) {
 //using the API data function
 
 function loadPage(data) {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
+    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then //?limit=25&offset=800`).then
         (async (data) => {
             for (const pokemon of data.results) {
                 await getAPIData(pokemon.url).then((pokeData) => {
                     populatePokeCard(pokeData)
-                })            
+                })
             }
 
         })
-   
+
 
 }
 
 const pokeGrid = document.querySelector('.pokemonGrid')
+const loadButton = document.querySelector('.load')
+const newPokemonButton = document.querySelector('.newPokemon')
+
+newPokemonButton.addEventListener ('click', () => {
+    let pokeName = prompt('what is your new Pokemon nmae?')
+    let newPokemon = new Pokemon(pokeName, 400, 150, ['code', 'sleep', 'code some more'])
+    console.log(newPokemon)
+
+})
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+    loadButton.disabled = true
+})
 
 function populatePokeCard(singlePokemon) {
-let pokeScene = document.createElement('div')
-pokeScene.className = 'scene'
-let pokeCard = document.createElement('div')
-pokeCard.className = 'card'
-pokeCard.addEventListener( 'click', function() {
-    pokeCard.classList.toggle('is-flipped')
-  })
-let pokeFront = document.createElement('div')
-let pokeBack = document.createElement('div')
-
-let frontLabel = document.createElement('p')
-frontLabel.textContent = singlePokemon.name
-let frontImage = document.createElement('img')
-frontImage.src = `../images/pokemon/${getImageFileName(singlePokemon)}.png`
-
-let backLabel = document.createElement('p')
-backLabel.textContent = `${singlePokemon.moves.length} moves`
-pokeBack.appendChild(backLabel)
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', function () {
+        pokeCard.classList.toggle('is-flipped')
+    })
 
 
-pokeFront.appendChild(frontImage)
-pokeFront.appendChild(frontLabel)
-pokeCard.appendChild(pokeFront)
-pokeCard.appendChild(pokeBack)
-pokeScene.appendChild(pokeCard)
-pokeGrid.appendChild(pokeScene)
+
+
+
+
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
+    pokeFront.appendChild(frontImage)
+    pokeFront.appendChild(frontLabel)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `${pokemon.moves.length} moves`
+    backLabel.addEventListener('click', () => getMovesDetails(pokemon.moves))
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
+
+function getMovesDetails(pokemonMoves) {
+    const movesUrl = pokemonMoves[0].moves.url
+    return getAPIData(movesUrl).then((data) => data.type)
 }
 
 function getImageFileName(pokemon) {
@@ -63,8 +95,16 @@ function getImageFileName(pokemon) {
         return `00${pokemon.id}`
     } else if (pokemon.id > 9 && pokemon.id < 100) {
         return `0${pokemon.id}`
-    } 
+    } else if (pokemon.id > 99 && pokemon.id < 810) {
+        return `${pokemon.id}`
+    }
+    return `pokeball`
 }
 
-
-loadPage()
+function Pokemon(name, height, weight, abilities) {
+    this.name = name
+    this.height = height
+    this.weight = weight
+    this.abilities = abilities
+    this.id = 900
+}
