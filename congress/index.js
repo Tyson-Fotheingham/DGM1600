49e1883abe34a8e322f5595e97393985593ec1b1
Tyson Fotheringham <tyson.fotheringham@gmail.com>
2020-12-11 23:56:29 -0700

    added congras page

---
+++ congress/index.js
@@ -0,0 +1,59 @@
+import { senators } from '../data/senators.js'
+import { removeChildren } from '../utils/index.js'
+
+const senatorGrid = document.querySelector('.senatorGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+birthdayButton.addEventListener('click', () => {
+    birthdaySort()
+})
+
+function populateSenatorDiv(simpleSenetors) {
+    removeChildren(senatorGrid)
+    simpleSenetors.forEach(senator => {
+        let senDiv = document.createElement('div')
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+        let partyIcon = document.createElement('i')
+        if (senator.party === 'R') partyIcon.classname = 'fas fa-republican'
+        if (senator.party === 'D') partyIcon.classname = 'fas fa-democrat'
+        if (senator.party === 'ID') partyIcon.classname = 'fas fa-star'
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        figCaption.appendChild(partyIcon)
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senDiv.appendChild(senFigure)
+        //senDiv.appendChild(progrssBars(senator))
+        senatorGrid.appendChild(senDiv)
+    })
+}
+
+function getSimlifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middleName ? ` ${senator.middleName} ` : ` `
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middleName}${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10),
+            missedVotesPct: senator.missed_votes_pct,
+            loyaltyPct: senator.votes_with_party_pct,
+            party: senator.party,
+            date_of_birth: parseInt(senator.date_of_birth, 10)
+        }
+    })
+}
+
+const mostSeniority = getSimlifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+
+function birthdaySort() {
+    populateSenatorDiv(getSimplifiedSenators(senators).sort((a, b) => {
+        return a.seniority - b.seniority
+    }))
+}
+
+populateSenatorDiv(getSimlifiedSenators(senators))
